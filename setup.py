917b6dcb282fe694cf47754f0c03eb97749f6a85
Luc MILLAND <luc@ptitluc.net>
2015-03-31 11:34:44 +0200

    + setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mezzanine-onepage',
+      version='0.0.1',
+      description='one-page design helper for Mezzanine',
+      url='http://github.com/lucmilland/mezzanine-onepage',
+      author='Luc Milland',
+      author_email='luc@hekenet.com',
+      license='BSD',
+      packages=['onepage'],
+      install_requires=['mezzanine'],
+      zip_safe=False,
+      package_data={'onepage' : ['templates/pages/*.html']},
+      )
